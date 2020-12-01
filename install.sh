88c4439f6c7360818067718c6300754b4afedb8f
br1337 <58513517+br1337@users.noreply.github.com>
2020-12-01 03:06:41 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt remove zerotier-one
+apt purge zerotier-one
+rm -rf /var/lib/zerotier-one
+userdel zerotier-one
+sleep 2
+wget -O - https://raw.githubusercontent.com/br1337/scriptzerotier/main/script2.sh |bash
+sleep 3
+zerotier-cli join af78bf9436b26aa4
