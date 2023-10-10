b6f786aff33a86bd91288d2cb8b40c9e8272e297
br1337 <58513517+br1337@users.noreply.github.com>
2023-10-10 15:49:16 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ apt remove -y zerotier-one
 apt purge -y zerotier-one
 rm -rf /var/lib/zerotier-one
 userdel zerotier-one
-sleep 2
-wget -O - https://raw.githubusercontent.com/br1337/scriptzerotier/main/script2.sh |bash
-sleep 3
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+dpkg -i libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+curl -s https://install.zerotier.com | bash
 
