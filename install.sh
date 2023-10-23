155d1bcd92b3f0e304e01e41726559e482581cee
br1337 <58513517+br1337@users.noreply.github.com>
2023-10-23 08:24:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ rm -rf /var/lib/zerotier-one
 userdel zerotier-one
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
 dpkg -i libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
-dpkg -i libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
+#wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
+#dpkg -i libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
 curl -s https://install.zerotier.com | bash
 
