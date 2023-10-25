f8b8b35fc1cd55e18ad0cd7e41c3edaa22a6bc13
br1337 <58513517+br1337@users.noreply.github.com>
2023-10-25 14:26:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ userdel zerotier-one
 apt install libssl1.1
 #wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
 #dpkg -i libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-№wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
-№dpkg -i libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
+wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
+dpkg -i libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
 curl -s https://install.zerotier.com | bash
 
