d3ad341568989006ee230fb7e43c8678a7c19a9e
br1337 <58513517+br1337@users.noreply.github.com>
2024-02-20 13:09:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,16 @@ apt remove -y zerotier-one
 apt purge -y zerotier-one
 rm -rf /var/lib/zerotier-one
 userdel zerotier-one
-apt install libssl1.1
-#wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-#dpkg -i libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
-dpkg -i libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
-curl -s https://install.zerotier.com | bash
+wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+# save /etc/debian-version
+# (which will be something like kali-rolling)
+DV_SAVE=$(cat /etc/debian_version)# pretend we're Debian buster
+echo buster | sudo tee /etc/debian_version >/dev/null
+# follow ZeroTier install instructions from:
+# https://www.zerotier.com/download/
+# for example, if you don't care about checking gpg signatures:
+curl -s https://install.zerotier.com | sudo bash
+# restore /etc/debian-version
+echo $DV_SAVE | sudo tee /etc/debian_version >/dev/null
 
